1caaded06f88830babe77d9448442ce30d4b852b
Aaron Parker <aaron@stealthpuppy.com>
2022-02-01 19:01:20 +1100

    Update Main.Tests.ps1

--- tests/Main.Tests.ps1
+++ tests/Main.Tests.ps1
@@ -16,44 +16,14 @@ Else {
     $projectRoot = Resolve-Path -Path (((Get-Item (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)).Parent).FullName)
 }
 
-# Set $VerbosePreference so full details are sent to the log; Make Invoke-WebRequest faster
-#$VerbosePreference = "Continue"
-$ProgressPreference = "SilentlyContinue"
-
 BeforeDiscovery {
+    # Set $VerbosePreference so full details are sent to the log; Make Invoke-WebRequest faster
+    $VerbosePreference = "Continue"
+    $ProgressPreference = "SilentlyContinue"
+
     # Get the scripts to test
     $Scripts = @(Get-ChildItem -Path $([System.IO.Path]::Combine($projectRoot, "Install-Apps.ps1")) -ErrorAction "SilentlyContinue")
     $testCase = $Scripts | ForEach-Object { @{file = $_ } }
-
-    # Get the ScriptAnalyzer rules
-    $scriptAnalyzerRules = Get-ScriptAnalyzerRule
-}
-
-
-# All scripts validation
-Describe "General project validation" {
-    It "Script <file.Name> should be valid PowerShell" -TestCases $testCase {
-        param ($file)
-
-        $file.FullName | Should -Exist
-
-        $contents = Get-Content -Path $file.FullName -ErrorAction Stop
-        $errors = $null
-        $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
-        $errors.Count | Should -Be 0
-    }
-
-    It "Script <file.Name> should pass ScriptAnalyzer" -TestCases $testCase {
-        param ($file)
-        $analysis = Invoke-ScriptAnalyzer -Path $file.FullName -ExcludeRule @("PSAvoidGlobalVars", "PSAvoidUsingWMICmdlet") -Severity @("Warning", "Error")
-
-        ForEach ($rule in $scriptAnalyzerRules) {
-            If ($analysis.RuleName -contains $rule) {
-                $analysis | Where-Object RuleName -EQ $rule -OutVariable failures | Out-Default
-                $failures.Count | Should -Be 0
-            }
-        }
-    }
 }
 
 # Per script tests
@@ -65,11 +35,7 @@ Describe "Script execution validation" -Tag "Windows" -ForEach $Scripts {
 
     Context "Validate <script.Name>." {
         It "<script.Name> should execute OK" {
-            Push-Location -Path $([System.IO.Path]::Combine($projectRoot))
-            Write-Host "Running script: $($script.FullName)."
-            $VerbosePreference = "Continue"
             & $script.FullName | Should -Not Throw
-            Pop-Location
         }
     }
 }
