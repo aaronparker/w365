4519a937efa611b9856cf17195d175f7f595f822
Aaron Parker <aaron@stealthpuppy.com>
2022-03-12 22:18:18 +1100

    Update `BeforeAll`; `Set-MpPreference`

--- Install.ps1
+++ Install.ps1
@@ -8,8 +8,8 @@ param()
 Set-ExecutionPolicy Bypass -Scope "Process" -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 
+Set-MpPreference -DisableRealtimeMonitoring $true
 $Scripts = @(
-    "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/01_Rds-PrepImage.ps1",
     "https://raw.githubusercontent.com/aaronparker/packer/main/build/common/03_RegionLanguage.ps1",
     "https://stealthpuppy.com/image-customise/Install.ps1",
     "https://raw.githubusercontent.com/aaronparker/packer/main/build/common/06_SupportFunctions.ps1",
@@ -24,3 +24,4 @@ $Scripts = @(
 ForEach ($Script in $Scripts) {
     Invoke-Expression -Command ((New-Object -TypeName "System.Net.WebClient").DownloadString($Script))
 }
+Set-MpPreference -DisableRealtimeMonitoring $false
