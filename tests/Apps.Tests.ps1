7d11d3c2e92da3b5ca2d19fb3a76fb6f8051d4fe
Aaron Parker <aaron@stealthpuppy.com>
2022-03-12 20:42:59 +1100

    Update Apps.Tests.ps1

--- tests/Apps.Tests.ps1
+++ tests/Apps.Tests.ps1
@@ -67,6 +67,7 @@ BeforeDiscovery {
 
     # Get the Software list; Output the installed software to the pipeline for Packer output
     $InstalledSoftware = Get-InstalledSoftware | Sort-Object -Property "Publisher", "Version"
+    Import-Module -Name "Evergreen" -Force
 
     # Get the list of software to test
     # Get path
@@ -76,6 +77,7 @@ BeforeDiscovery {
     Else {
         $Path = $env:GITHUB_WORKSPACE
     }
+    Write-Host "Read input file: $([System.IO.Path]::Combine($Path, "tests", "Apps.json"))."
     $Applications = Get-Content -Path $([System.IO.Path]::Combine($Path, "tests", "Apps.json")) | ConvertFrom-Json
 }
 
@@ -85,17 +87,18 @@ Describe -Name "Validate installed <App.Name>" -ForEach $Applications {
 
         # Get details for the current application
         $App = $_
+        Write-Host "Getting details for $($App.Name)."
         $Latest = Invoke-Expression -Command $App.Filter
         $Installed = $InstalledSoftware | Where-Object { $_.Name -eq $App.Installed } | Select-Object -First 1
     }
 
-    Context "Validate <application> is installed" {
+    Context "Validate <App.Installed> is installed" {
         It "Should be a valid object" {
             $Installed | Should -Not -BeNullOrEmpty
         }
     }
 
-    Context "Validate <application> version" {
+    Context "Validate <App.Installed> version" {
         It "Should be the current version or better" {
             [System.Version]$Installed.Version | Should -BeGreaterOrEqual ([System.Version]$Latest.Version)
         }
