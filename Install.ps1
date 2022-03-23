44f6e025ab2a6da5be551c36804827ec471cac5b
Aaron Parker <aaron@stealthpuppy.com>
2022-03-24 09:52:22 +1100

    Update script paths; Add tests for script paths

--- Install.ps1
+++ Install.ps1
@@ -8,11 +8,14 @@ param()
 Set-ExecutionPolicy Bypass -Scope "Process" -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 
+# Disable Defender during install for speed
 Set-MpPreference -DisableRealtimeMonitoring $true
+
+# Run the script to install applications
 $Scripts = @(
-    "https://raw.githubusercontent.com/aaronparker/packer/main/build/common/03_RegionLanguage.ps1",
+    "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/03_RegionLanguage.ps1",
     "https://stealthpuppy.com/image-customise/Install.ps1",
-    "https://raw.githubusercontent.com/aaronparker/packer/main/build/common/06_SupportFunctions.ps1",
+    "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/06_SupportFunctions.ps1",
     "https://vcredist.com/install.ps1",
     "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/08_MicrosoftFSLogixApps.ps1",
     "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/09_MicrosoftEdge.ps1",
@@ -21,7 +24,15 @@ $Scripts = @(
     "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/12_MicrosoftOneDrive.ps1",
     "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/39_AdobeAcrobatReaderDC.ps1"
 )
-ForEach ($Script in $Scripts) {
-    Invoke-Expression -Command ((New-Object -TypeName "System.Net.WebClient").DownloadString($Script))
+foreach ($Script in $Scripts) {
+    
+    try {
+        Invoke-Expression -Command ((New-Object -TypeName "System.Net.WebClient").DownloadString($Script))
+    }
+    catch {
+        Write-Error -Message $_.Exception.Message
+    }
 }
+
+# Re-enable Defender
 Set-MpPreference -DisableRealtimeMonitoring $false
---
+++ tests/Scripts.Tests.ps1
@@ -0,0 +1,40 @@
+<#
+    .SYNOPSIS
+        Use Pester and Evergreen to validate installed apps.
+#>
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingInvokeExpression", "")]
+[CmdletBinding()]
+param()
+
+BeforeDiscovery {
+
+    $Scripts = @(
+        "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/03_RegionLanguage.ps1",
+        "https://stealthpuppy.com/image-customise/Install.ps1",
+        "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/06_SupportFunctions.ps1",
+        "https://vcredist.com/install.ps1",
+        "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/08_MicrosoftFSLogixApps.ps1",
+        "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/09_MicrosoftEdge.ps1",
+        "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/10_Microsoft365Apps.ps1",
+        "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/11_MicrosoftTeams.ps1",
+        "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/12_MicrosoftOneDrive.ps1",
+        "https://raw.githubusercontent.com/aaronparker/packer/main/build/rds/39_AdobeAcrobatReaderDC.ps1"
+    )
+}
+
+# Per script tests
+Describe -Name "Validate script <Script>" -ForEach $Scripts {
+    BeforeAll {
+
+        # Get details for the current application
+        $Script = $_
+    }
+
+    Context "Validate <Script> exists" {
+        It "Should be a valid URL" {
+            {try { $r = (New-Object -TypeName "System.Net.WebClient").DownloadString($Script) } catch { Throw }} | Should -Not -Throw
+        }
+    }
+}
